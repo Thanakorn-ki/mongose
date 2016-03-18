28bc0e44e2f4b23a67e6f9ee6364aade30f56841
salapao2136 <potook_02@hotmail.com>
2016-03-19 00:07:23 +0700

    Added files via upload

--- public/app.js
+++ public/app.js
@@ -1,23 +1,67 @@
 angular.module('mid', [])
     .controller('midcontron', function($http, $scope) {
-
-        $scope.save = function() {
-            var data = { name: $scope.name, surname: $scope.surname, age: $scope.age }
-            $http.post("/save", data)
-                .then(
-                    function(res) {
-                        console.log('save');
-                    },
-                    function(err) { console.log(err); }
-                )
-
-        }
         $scope.find = []
         $http.get("/findall").then(
-            function(res) {
+                function(res) {
 
-                $scope.find = res.data
-                console.log($scope.find);
+                    $scope.find = res.data
+                        // console.log($scope.find);
+                }
+            )
+            //////////////////////
+
+        $scope.save = function() {
+                var data = { name: $scope.name, surname: $scope.surname, age: $scope.age }
+                $http.post("/save", data)
+                    .then(
+                        function(res) {
+                            console.log('save');
+                            $scope.find.push(data)
+
+                        },
+                        function(err) { console.log(err); }
+                    )
             }
-        )
+            /////////////////////////////////
+
+        $scope.delete = function(_id, index) {
+                console.log(_id);
+                var id = _id
+                $http.delete("/delete/" + _id).then(
+                    function(res) {
+                        console.log(res.data);
+                    })
+                 $scope.find.splice(index, 1);
+            }
+            //////////////////////////////////
+        $scope.sub = true
+        $scope.index
+        $scope.update = function(_id, index) {
+                $scope.index = index
+                console.log(index);
+                $http.get("/findall/" + _id).then(
+                    function(res) {
+                        $scope.newfind = res.data
+                        $scope.name = res.data[0].name
+                        $scope.surname = res.data[0].surname
+                        $scope.age = res.data[0].age
+                        $scope.sub = false
+                            //console.log($scope.age);
+                    }
+
+                )
+            }
+            ////////////////////////////////
+        $scope.confirmup = function() {
+            // console.log($scope.newfind[0]._id); 
+            var data = { _id: $scope.newfind[0]._id, name: $scope.name, surname: $scope.surname, age: $scope.age }
+            console.log(data);
+            $http.put("/putupdate", data).then(
+                function(res) {
+                    console.log(res.data);
+                    $scope.find[$scope.index] = data
+                }, // compleate
+                function(err) { console.log(err.data); } // error
+            )
+        }
     });
--- public/server.js
+++ public/server.js
@@ -17,13 +17,13 @@ var kittySchema = mongoose.Schema({
     age: Number
 });
 
-/////// ต้อง เติ่ม S =>  collections
+/////// ต้อง เติ่ม S =>  ชื่อ ืี่อยู่ใน database collections
 var collections = mongoose.model('collections', kittySchema);
 collections.find(function(err, collection) {
-    if (err) return console.error(err);
-    console.log(collection);
-})
-/////////////////////////////////////////
+        if (err) return console.error(err);
+        //console.log(collection);
+    })
+    /////////////////////////////////////////
 router.post('/save', jsonParser, function(req, res) {
         var full = {
             name: req.body.name,
@@ -32,25 +32,47 @@ router.post('/save', jsonParser, function(req, res) {
         }
         var fluffy = new collections(full);
         fluffy.save(function(err, collection) {
-            if (err) return console.error(err);
-
+            if (err) return res.send(err);
         });
-        res.send(full);
-        console.log('TestPOST-OK')
+         res.send(full);
     })
     ////////////////////////
 router.get('/findall', function(req, res) {
 
         collections.find(function(err, collections) {
             if (err) return console.error(err);
-            console.log(collections);
+            // console.log(collections);
             res.send(collections);
         })
-
-
     })
-    //////////
-
-
+    ////////////////////////
+router.get('/findall/:_id', function(req, res) {
+        //console.log(req.params._id);
+        var data = { _id: req.params._id }
+        collections.find(data, function(err, collections) {
+            if (err) return console.error(err);
+            // console.log(collections);
+            res.send(collections);
+        })
+    })
+    ////////////////////////
+router.delete('/delete/:id', jsonParser, function(req, res) {
+        // var data = {_id:req.params.id}
+        collections.remove({ _id: req.params.id }, function(err, collections) {
+            if (err) return console.error(err);
+            // console.log(collections);
+            res.send(collections);
+        })
+    })
+    ////////////////////////
+router.put('/putupdate', jsonParser, function(req, res) {
+    var data = { _id: req.body._id, name: req.body.name, surname: req.body.surname, age: req.body.age }
+    console.log(data);
+    collections.update({ _id: req.body._id }, data, function(err, collections) {
+        if (err) return console.error(err);
+        // console.log(collections);
+        res.send(collections);
+    })
+})
 
 module.exports = router
